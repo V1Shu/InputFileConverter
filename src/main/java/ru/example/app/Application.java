cb8eb2d98b5e7fbc06693ba2b6e915f56c0c787c
vadim_sh@bk.ru <vadim_sh@bk.ru>
2021-03-18 15:56:30 +0200

    reload project

--- src/main/java/ru/example/app/Application.java
+++ src/main/java/ru/example/app/Application.java
@@ -3,12 +3,10 @@ package ru.example.app;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import ru.example.app.configuration.ApplicationConfiguration;
 import ru.example.app.model.FileData;
 import ru.example.app.services.ConvertService;
 
-@Configuration
 @ComponentScan("ru.example.app.services")
 public class Application {
 
